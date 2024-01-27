8d07ca1bc3ab0a2f9ac517e8aa59a1620680bb05
Joaquim d'Souza <joaquimds@gmail.com>
2024-01-27 14:31:23 +0100

    feat: improve spinner, add default order for authors and tags

--- collection/models.py
+++ collection/models.py
@@ -3,12 +3,18 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=128)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=128)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
